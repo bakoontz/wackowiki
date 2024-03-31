e01e9e4b34e8da32dff898eac667489608a53108
Brian Koontz <brianko@gmail.com>
2024-03-30 22:21:48 -0500

    Checkpoint

---
+++ src/plugins/action/vm_add_passage.php
@@ -0,0 +1,64 @@
+<?php
+
+if (!defined('IN_WACKO'))
+{
+	exit;
+}
+
+$info = <<<EOD
+Description:
+	Add a new passage for a given resource that is attached to the parent of this page
+Usage:
+	{{vm_add_passage}}
+EOD;
+
+// set defaults
+$help	??= 0;
+
+if ($help)
+{
+	$tpl->help	= $this->help($info, 'vm_add_passage');
+	return;
+}
+
+if(@$_POST['_action'] === 'vm_add_passage') {
+    $parent_id = $this->get_parent_id();
+    if(null == $parent_id) {
+        $this->set_message($this->_t("Cannot find parent page"), 'error');
+        $this->reload_me();
+    }
+    // By convention, a single verse will have identical start/end
+    // chapter/verse
+    $chapter_end = $_POST['chapter_end'];
+    if(0 == $chapter_end || null == $chapter_end) {
+        $chapter_end = $_POST['chapter_start'];
+    }
+    $verse_end = $_POST['verse_end'];
+    if(0 == $verse_end || null == $verse_end) {
+        $verse_end = $_POST['verse_start'];
+    }
+    // Create DB records
+    $sql = 'INSERT INTO ' . 'vm_passage SET ' .
+        'book_id = (SELECT id FROM vm_book WHERE book_abbrev = \'' . $_POST['book'] . '\'), ' . 
+        'chapter_start    = \'' . $_POST['chapter_start'] . '\', ' .
+        'verse_start      = \'' . $_POST['verse_start']  . '\', ' .
+        'chapter_end      = \'' . $chapter_end . '\', ' .
+        'verse_end        = \'' . $verse_end . '\' ';
+    if(null == $this->db->sql_query($sql, 2)) {
+        $this->set_message($this->_t("Insert into vm_passage failed: ") . $sql, 'error');
+        $this->reload_me();
+    }
+    $sql = 'INSERT INTO ' . 'vm_passage_resource SET ' .
+        'passage_id       = LAST_INSERT_ID(), ' .
+        'resource_id = (SELECT id FROM vm_resource WHERE page_id = \'' . $parent_id . '\'), ' . 
+        'page             = \'' . $_POST['page']  . '\', ' .
+        'notes            = \'' . $_POST['notes'] . '\' ';
+    if(null == $this->db->sql_query($sql, 2)) {
+        $this->set_message($this->_t("Insert into vm_passage_resource failed: ") . $sql, 'error');
+        $this->reload_me();
+    }
+
+    $this->reload_me();
+} else {
+	$tpl->l_href		= $this->href();
+}
