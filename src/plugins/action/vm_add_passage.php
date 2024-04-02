a74529b42a4416f6f0e105bd6d50f4d6c72a2647
Brian Koontz <brianko@gmail.com>
2024-04-02 03:26:45 -0500

    Refactor, more integrity checks

--- src/plugins/action/vm_add_passage.php
+++ src/plugins/action/vm_add_passage.php
@@ -21,6 +21,56 @@ if ($help)
 	return;
 }
 
+$get_last_insert_id = function() {
+    $sql = "SELECT LAST_INSERT_ID()";
+    $result = $this->db->load_single($sql);
+    if(null != $result) {
+        return $result['LAST_INSERT_ID()'];
+    } else {
+        return null;
+    }
+};
+
+$get_passage = function() {
+    $sql = "SELECT id FROM vm_passage
+        WHERE book_id = (SELECT id FROM vm_book WHERE book_abbrev = '{$_POST['book']}')
+        AND chapter_start = {$_POST['chapter_start']}
+        AND verse_start = {$_POST['verse_start']}
+        AND chapter_end = {$_POST['chapter_end']}
+        AND verse_end = {$_POST['verse_end']}";
+    $result = $this->db->load_single($sql);
+    if(null != $result) {
+        return $result['id'];
+    } else {
+        return null;
+    }
+};
+
+$add_passage = function() {
+    $sql = 'INSERT INTO ' . 'vm_passage SET ' .
+        'book_id = (SELECT id FROM vm_book WHERE book_abbrev = \'' . $_POST['book'] . '\'), ' . 
+        'chapter_start    = \'' . $_POST['chapter_start'] . '\', ' .
+        'verse_start      = \'' . $_POST['verse_start']  . '\', ' .
+        'chapter_end      = \'' . $_POST['chapter_end'] . '\', ' .
+        'verse_end        = \'' . $_POST['verse_end'] . '\' ';
+    if(null == $this->db->sql_query($sql, 2)) {
+        $this->set_message($this->_t("Insert into vm_passage failed: ") . $sql, 'error');
+        $this->reload_me();
+    }
+};
+
+$add_passage_resource = function($passage_id, $parent_id) {
+    $sql = 'INSERT INTO ' . 'vm_passage_resource SET ' .
+        'passage_id       = \'' . $passage_id . '\', ' .
+        'resource_id = (SELECT id FROM vm_resource WHERE page_id = \'' . $parent_id . '\'), ' . 
+        'page             = \'' . $_POST['page']  . '\', ' .
+        'notes            = \'' . $_POST['notes'] . '\' ';
+    if(null == $this->db->sql_query($sql, 2)) {
+        $this->set_message($this->_t("Insert into vm_passage_resource failed: ") . $sql, 'error');
+        $this->reload_me();
+    }
+};
+
 if(@$_POST['_action'] === 'vm_add_passage') {
     $parent_id = $this->get_parent_id();
     if(null == $parent_id) {
@@ -31,33 +81,19 @@ if(@$_POST['_action'] === 'vm_add_passage') {
     // chapter/verse
     $chapter_end = $_POST['chapter_end'];
     if(0 == $chapter_end || null == $chapter_end) {
-        $chapter_end = $_POST['chapter_start'];
+        $_POST['chapter_end'] = $_POST['chapter_start'];
     }
     $verse_end = $_POST['verse_end'];
     if(0 == $verse_end || null == $verse_end) {
-        $verse_end = $_POST['verse_start'];
+        $_POST['verse_end'] = $_POST['verse_start'];
     }
-    // Create DB records
-    $sql = 'INSERT INTO ' . 'vm_passage SET ' .
-        'book_id = (SELECT id FROM vm_book WHERE book_abbrev = \'' . $_POST['book'] . '\'), ' . 
-        'chapter_start    = \'' . $_POST['chapter_start'] . '\', ' .
-        'verse_start      = \'' . $_POST['verse_start']  . '\', ' .
-        'chapter_end      = \'' . $chapter_end . '\', ' .
-        'verse_end        = \'' . $verse_end . '\' ';
-    if(null == $this->db->sql_query($sql, 2)) {
-        $this->set_message($this->_t("Insert into vm_passage failed: ") . $sql, 'error');
-        $this->reload_me();
+    // Create DB records (if needed)
+    $passage_id = $get_passage();
+    if(null == $passage_id) { 
+        $add_passage($parent_id);
+        $passage_id = $get_last_insert_id();
     }
-    $sql = 'INSERT INTO ' . 'vm_passage_resource SET ' .
-        'passage_id       = LAST_INSERT_ID(), ' .
-        'resource_id = (SELECT id FROM vm_resource WHERE page_id = \'' . $parent_id . '\'), ' . 
-        'page             = \'' . $_POST['page']  . '\', ' .
-        'notes            = \'' . $_POST['notes'] . '\' ';
-    if(null == $this->db->sql_query($sql, 2)) {
-        $this->set_message($this->_t("Insert into vm_passage_resource failed: ") . $sql, 'error');
-        $this->reload_me();
-    }
-
+    $add_passage_resource($passage_id, $parent_id);
     $this->reload_me();
 } else {
 	$tpl->l_href		= $this->href();
